0f335d60c3b69b6549e42efeaa8fad61fb9cf17f
sms01-01 <mouhamedsy.01@gmail.com>
2025-09-15 19:52:54 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+theme: {
+  extend: {
+    colors: {
+      'brand-blue': '#eaf6fb',
+      'brand-yellow': '#ffe066',
+      // etc.
+    },
+    fontFamily: {
+      'sans': ['Inter', 'ui-sans-serif', 'system-ui'],
+    }
+  }
+}
